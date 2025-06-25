7d42bd14fdf9c61949c85980f02c43a0d90c5b72
fraktc <francy.contib@gmail.com>
2025-06-25 20:10:04 +0200

    dummy solve

--- src/cp/solve.py
+++ src/cp/solve.py
@@ -221,6 +221,13 @@ def solve(instance, timeout, cache={}, random_seed=42, models_filter=None, **kwa
     instance_path = os.path.join(pathlib.Path(__file__).parent.resolve(), ".instance.dzn")
     out_results = {}
 
+    return {
+        "time": -1,
+        "optimal": None,
+        "obj": -1,
+        "sol": None,
+    }
+
     for experiment in experiments_setup:
         if (models_filter is not None) and (experiment["name"] not in models_filter):
             continue
